ea2325bf07974dc84c5d0131a8f001d79ee5b124
Sandra To <sandra.to.e@gmail.com>
2021-05-16 20:43:31 -0700

    Refactor code to take in user birthday from terminal input

--- practices/check-birthday/Program.cs
+++ practices/check-birthday/Program.cs
@@ -6,53 +6,38 @@ namespace CheckBirthday
   {
     static void Main(string[] args)
     {
-      //   DateTime today = DateTime.Today;
-      DateTime today = new DateTime(2021, 5, 16);
-      DateTime birthday = new DateTime(2002, 4, 16);
+      Console.WriteLine("========= BIRTHDAY CHECK! ========");
 
-      // If input = 4, 14 -- should equal 333 days
-      // If input = 6, 14 -- should equal 28 days
-      // If input = 4, 17 -- should equal 336 days
-      // If input = 6, 17 -- should equal 31 days
-
-      Console.WriteLine(today);
-      Console.WriteLine(birthday);
+      DateTime today = DateTime.Today;
 
       int currentMonth = today.Month;
       int currentDate = today.Day;
 
-      int birthMonth = birthday.Month;
-      int birthDate = birthday.Day;
+      Console.Write("Please enter the two digit value of your birth month: ");
+      int birthMonth = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Please enter the two digit value of your birth day: ");
+      int birthDate = Convert.ToInt32(Console.ReadLine());
 
       bool correctMonth = currentMonth == birthMonth;
-
       bool correctDate = currentDate == birthDate;
 
       if (correctMonth && correctDate)
       {
-        Console.WriteLine("***Happy Birthday!***");
+        Console.WriteLine("*** It's your birthday today!! Happy Birthday! 🥳 ***");
       }
       else
       {
         int daysUntil = currentDate - birthDate;
 
-        Console.WriteLine("daysUntil {0}", daysUntil);
-
         int monthsUntil = currentMonth - birthMonth;
 
-
-        Console.WriteLine("monthsUntil {0}", monthsUntil);
-
         int totalDaysDiff = (monthsUntil * 30) + daysUntil;
-
-        Console.WriteLine($"totalDaysDiff {totalDaysDiff}");
-
         // if negative diff, means upcoming birthday within the year
         // if positive diff, means it already passed
-
         int totalDaysUntil = totalDaysDiff > 0 ? 365 - totalDaysDiff : Math.Abs(totalDaysDiff);
 
-        Console.WriteLine($"There are {totalDaysUntil} days until your birthday!");
+        Console.WriteLine($"*** There are {totalDaysUntil} days until your birthday! 🎂 ***");
       }
     }
   }
