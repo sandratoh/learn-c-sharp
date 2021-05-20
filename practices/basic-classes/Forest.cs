06fcb2f97e9b273451fe4f60d3d782094bc5f1b1
Sandra To <sandra.to.e@gmail.com>
2021-05-20 00:59:55 -0700

    Add fields to forest class

---
+++ practices/basic-classes/Forest.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BasicClasses
+{
+  class Forest
+  {
+    // Fields -- pieces of class data
+    public string name;
+    public int trees;
+    public int age;
+    public string biome;
+  }
+}
---
+++ practices/basic-classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest f = new Forest();
+      f.name = "Amazon";
+      f.trees = 1123;
+      f.age = 200;
+      f.biome = "tropical rainforest";
+
+      Console.WriteLine(f.name);
+    }
+  }
+}
