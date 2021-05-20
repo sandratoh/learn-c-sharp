345008386efd02b7f41261fbd5f10ffad989f535
Sandra To <sandra.to.e@gmail.com>
2021-05-20 00:28:59 -0700

    Use foreach loop to iterate through number array for odd and even #

---
+++ practices/loop-number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EvenOrOddLoop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] numbers = { 1, 3, 4, 6, 7, 9, 11, 12 };
+
+      foreach (int number in numbers)
+      {
+        // Must assign to variable -- not all expressions can be statements in C#
+        string result = number % 2 == 0 ? "EVEN" : "ODD";
+
+        Console.WriteLine(result);
+      }
+    }
+  }
+}
